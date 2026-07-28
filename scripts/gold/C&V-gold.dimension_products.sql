f106d9bd321c271e46199f175e67b2b54884309e
sanjai570 <sanjaimarshy12456@gmail.com>
2026-07-29 00:34:16 +0530

    gold dimensions are craeted

---
+++ scripts/gold/C&V-gold.dimension_products.sql
@@ -0,0 +1,17 @@
+CREATE VIEW gold.dim_products AS
+SELECT 
+ROW_NUMBER() OVER(ORDER BY pn.prd_start_dt , pn.prd_key) AS product_key,
+    pn.prd_id AS product_id,
+    pn.prd_key AS product_number,
+    pn.prd_nm AS product_name,
+    pn.cat_id as category_id,
+    pc.cat AS category,
+    pc.subcat as subcategory,
+    pc.maintenance,
+    pn.prd_cost as cost,
+    pn.prd_line product_line,
+    pn.prd_start_dt as start_date
+FROM silver.crm_prd_info AS pn
+LEFT JOIN silver.erp_px_cat_g1v2 pc
+ON pn.cat_id = pc.id
+WHERE prd_end_dt IS NULL
